45d02d508acdce18df5509f3270a84de2b2232e3
Nikita Mogilevsky <nikitamog@gmail.com>
2018-10-25 13:16:29 -0700

    Some minor changes

--- basicoutput.java
+++ basicoutput.java
@@ -4,7 +4,24 @@ public class basicoutput {
     
     public static void main(String[] args) {
 	System.out.println("Hello world!");
+        System.out.println();
+        bob(5);
+        System.out.println();
+        bob(7);
+
+        int a, b, c;
+        a = 5;
+        b = a;
+        c = a;
+        a = 4;
+        System.out.println("C'est <<c>>: " + c);
     }
     
-
+    public static void bob(int b) {
+        if (b < 3) {
+            return;
+        }
+        System.out.println(b*b);
+        return;
+    }
 }
